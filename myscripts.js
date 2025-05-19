0858c94bbdbc9f93fd3703f86b7de7b0d749f109
Youlun <127579610+youlun@users.noreply.github.com>
2025-05-18 22:52:41 -0400

    Completed basic working ui

--- myscripts.js
+++ myscripts.js
@@ -1,44 +1,51 @@
-function playGame() {
-    function getComputerChoice() {
-        let choice = Math.floor(Math.random() * 3);
-        switch (choice) {
-            case 0:
-                return "rock";
-                break;
-            case 1:
-                return "scissors";
-                break;
-            case 2:
-                return "paper";
-                break;
-        }
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "scissors";
+            break;
+        case 2:
+            return "paper";
+            break;
     }
-
-    function getHumanChoice() {
-        return prompt("Choose rock, paper, or scissors").toLowerCase();
-    }
-
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound() {
-        const h = getHumanChoice();
-        const c = getComputerChoice();
-        if ((h === 'rock' && c === 'scissors') ||
-            (h === 'scissors' && c === 'paper') ||
-            (h === 'paper' && c === 'rock')) {
-            humanScore++;
-            console.log(`You Win! ${h} beats ${c}`);
-        } else {
-            computerScore++;
-            console.log(`You Lose! ${c} beats ${h}`);
-        }
-        console.log(`current score is human ${humanScore} and computer ${computerScore}`);
-    }
-
-
-
-    playRound();
 }
 
-playGame();
+let humanScore = 0;
+let computerScore = 0;
+
+const btnRock = document.querySelector("#Rock");
+const btnPaper = document.querySelector("#Paper");
+const btnScissors = document.querySelector("#Scissors");
+
+btnRock.addEventListener("click", () => {
+    playRound("rock");
+});
+btnPaper.addEventListener("click", () => {
+    playRound("paper");
+});
+btnScissors.addEventListener("click", () => {
+    playRound("scissors");
+});
+
+const scoreBoard = document.querySelector("#scoreBoard");
+const scoreNow = document.querySelector("#scoreNow");
+
+
+function playRound(selection) {
+    const h = selection;
+    const c = getComputerChoice();
+    if ((h === 'rock' && c === 'scissors') ||
+        (h === 'scissors' && c === 'paper') ||
+        (h === 'paper' && c === 'rock')) {
+        humanScore++;
+        scoreBoard.textContent = `You Win! ${h} beats ${c}`;
+    } else {
+        computerScore++;
+        scoreBoard.textContent = `You Lose! ${c} beats ${h}`;
+    }
+    scoreNow.textContent = `current score is human ${humanScore} and computer ${computerScore}`;
+}
